6f9211afdefcd040903360ca4d5b318e6860fb64
Marcello Seri <marcello.seri@gmail.com>
2024-12-08 23:04:23 +0100

    Add more functions
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function generateSeries() {
     document.getElementById("targetNumber").value,
   );
   const numTerms = parseInt(document.getElementById("numTerms").value);
+  const sequenceType = document.getElementById("sequenceType").value;
   const resultDiv = document.getElementById("result");
   resultDiv.innerHTML = ""; // Clear previous results
 
@@ -21,24 +22,26 @@ function generateSeries() {
   let term = 0;
   let termsCount = 0;
 
+  let sequence = getSequence(sequenceType);
+
   while (termsCount < numTerms) {
     if (currentSum < targetNumber) {
-      term = positiveTerm;
-      currentSum += 1 / term;
+      term = sequence(positiveTerm);
+      currentSum += term;
       positiveTerm += 2;
     } else {
-      term = negativeTerm;
-      currentSum -= 1 / term;
+      term = sequence(negativeTerm);
+      currentSum += term;
       negativeTerm += 2;
     }
     partialSums.push(currentSum);
-    series.push(1 / term);
+    series.push(term);
     termsCount++;
   }
 
-  resultDiv.innerHTML = `<p>The first ${numTerms} of the rearranged alternating harmonic series that converges to ${targetNumber} are:</p>`;
+  resultDiv.innerHTML = `<p>The first ${numTerms} terms of the rearranged ${getSequenceName(sequenceType)} series that converges to ${targetNumber} are:</p>`;
   resultDiv.innerHTML += `<p>${series.join(", ")}</p>`;
-  resultDiv.innerHTML += `<p>The corresponding first ${numTerms} partial sums of the rearranged alternating harmonic series that converges to ${targetNumber} are:</p>`;
+  resultDiv.innerHTML += `<p>The corresponding first ${numTerms} partial sums of the rearranged ${getSequenceName(sequenceType)} series that converges to ${targetNumber} are:</p>`;
   resultDiv.innerHTML += `<p>${partialSums.join(", ")}</p>`;
 
   // Destroy the existing chart if it exists
@@ -87,4 +90,30 @@ function generateSeries() {
       },
     },
   });
+}
+
+function getSequence(sequenceType) {
+  switch (sequenceType) {
+    case "harmonic":
+      return (k) => (-1) ** (k + 1) / k;
+    case "logarithmic":
+      return (k) => ((-1) ** (k + 1) * Math.log(k)) / k;
+    case "sqrt":
+      return (k) => (-1) ** (k + 1) / Math.sqrt(k);
+    default:
+      return (k) => (-1) ** (k + 1) / k;
+  }
+}
+
+function getSequenceName(sequenceType) {
+  switch (sequenceType) {
+    case "harmonic":
+      return "alternating harmonic";
+    case "logarithmic":
+      return "alternating logarithmic";
+    case "sqrt":
+      return "alternating square root";
+    default:
+      return "alternating harmonic";
+  }
 }
